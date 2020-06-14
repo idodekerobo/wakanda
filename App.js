89a2e839eb903527a4a4d49b2fe62530169a8deb
Idode Kerobo <idodekerobo@Idodes-MacBook-Pro.local>
2020-06-13 19:21:00 -0700

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import CREDENTIALS from './credentials'
+
+/*
+const instructions = Platform.select({
+ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
+android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+});
+*/
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
+   mapStyle: {
+      // width: Dimensions.get('window'),
+      // height: Dimensions.get('window').height
+   },
 });
+
+export default class App extends React.Component {
+   render() {
+      return (
+         <View style={styles.container}>
+            <Text>this is the map</Text>
+            <MapView style={styles.mapStyle}/>
+         </View>
+      );
+   }
+}
