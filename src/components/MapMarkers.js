6f24439c49b4ecfa3e150339b0646046ae4a5dd8
Idode Kerobo <idode.kerobo@gmail.com>
2021-05-14 14:58:41 -0500

    trying to get correct star pin size to render

--- src/components/MapMarkers.js
+++ src/components/MapMarkers.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Marker, Callout } from 'react-native-maps';
-import { Text, TouchableOpacity } from 'react-native'; // have to import TouchableOpacity from here
+import { Text, TouchableOpacity, Image } from 'react-native'; // have to import TouchableOpacity from here
 import { GlobalContext } from '../context/GlobalState';
 
 const MapMarkers = ({ biz, onCalloutPress }) => {
@@ -11,9 +11,13 @@ const MapMarkers = ({ biz, onCalloutPress }) => {
       longitude: 3.1438688,
    }
 
+   // const imageSource = <Image style={{width: 50, height: 50}} source={{uri: '../assets/star-pin'}} />;
+   // const imageString = '../../assets/Star_Pin-03-final.png'
+   const imageString = '../assets/Star_Pin-03-final.png';
    if (state.pinnedBusinessIds.includes(biz._id)) {
       return (
-         <Marker image={require('../../assets/Star_Pin-03-final.png')} stopPropagation={false} coordinate={(biz.coordinates) ? {latitude: biz.coordinates.latitude, longitude: biz.coordinates.longitude} : dummyCoordinates} onCalloutPress={onCalloutPress} pinColor="#0a431d">
+         // <Marker image={<Image style={{width: 50, height: 50}} source={{uri: '../assets/star-pin'}} />} stopPropagation={false} coordinate={(biz.coordinates) ? {latitude: biz.coordinates.latitude, longitude: biz.coordinates.longitude} : dummyCoordinates} onCalloutPress={onCalloutPress} pinColor="#0a431d">
+         <Marker image={require(imageString)} stopPropagation={false} coordinate={(biz.coordinates) ? {latitude: biz.coordinates.latitude, longitude: biz.coordinates.longitude} : dummyCoordinates} onCalloutPress={onCalloutPress} pinColor="#0a431d">
             <Callout key={biz._id}>
                <TouchableOpacity >
                   <Text>{biz.name}</Text>
